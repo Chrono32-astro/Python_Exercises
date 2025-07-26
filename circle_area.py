9c8da073b627d916384e0c12cce9fe16e6c7929c
Chrono32 <rufaro367@gmail.com>
2025-07-26 22:03:43 +0200

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,7 @@
+#Area of cirle
+import math
+
+radius = float(input("What is the radius of the circle? : "))
+area = math.pi*(radius**2)
+
+print ( f" The area of the circle is {round(area, 2)} cm^2 ")
