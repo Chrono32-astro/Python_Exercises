6d60c0e6cd914c24572c076dcb5d4d604c2beb73
Chrono32 <rufaro367@gmail.com>
2025-08-02 16:54:01 +0200

    Update 19_Leap_year.py
    Removed some code

--- 19_Leap_year.py
+++ 19_Leap_year.py
@@ -11,18 +11,3 @@ if (year % 4) == 0:
 else:
     print("{0} is not a leap year".format(year))
     
-   
-#Time counter
-
-import time
-
-res = int(input ("Start : "))
-out = int(input ("End : "))
-
-def counter (end, start = 0) :
-	for x in range (start , end+1 ) :
-		print (x)
-		time.sleep (1)
-	print ("DONE")
-
-counter (out)
